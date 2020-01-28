8ab39b87261ef4a532c2aea9f666f7e23d9ee7b3
ninja2002a <52730345+ninja2002a@users.noreply.github.com>
2020-01-28 10:49:07 -0800

    Add files via upload

---
+++ tree.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "math.cpp"
+
+using namespace std;
+
+Tree* Tree::getMath() {
+  return bin;
+}
+
+Tree* Tree::setMath() {
+  this -> bin = bin;
+}
---
+++ tree.h
@@ -0,0 +1,17 @@
+#ifndef TREE.H
+#define TREE.H
+
+using namespace std;
+
+class Tree {
+ public:
+  Tree* getMath();
+  Tree* setMath();
+  
+ private:
+  Tree* bin
+  
+};
+
+#endif
+
