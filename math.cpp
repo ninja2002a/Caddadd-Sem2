2d726ea353c2afec8fe510c2dab823b83c737574
ninja2002a <52730345+ninja2002a@users.noreply.github.com>
2020-02-13 10:48:27 -0800

    Add files via upload

---
+++ math.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <cstring>
+#include "tree.h"
+
+/*
+- Peek should be the only function, rest in code
+- Fix the repeat
+- Figure out how to make it read the equation so it knows where to place them
+- Make tree.cpp do something, it is useless
+ */
+
+using namespace std;
+//alt + x + linum-mode
+
+void enqueue();
+int dequeue();
+int peek(char*);
+
+int main() {
+  Tree* t = new Tree();
+  char input[100];
+  char list[1000];
+  char* num;
+  Tree* q;
+  Tree* dq = NULL;
+  Tree* wack = NULL;
+
+  //clears out the data that was already in list
+  for (int a = 0; a < 100; a++) {
+    list[a] == 0;
+  }
+
+  while(true) {
+    cout << "------------------------" << endl << "Enter an equation." << endl;
+    cin >> input;
+
+    //Checks if only numbers and operators
+    for (int i = 0; i < 100; i++) {
+      //cout << "Char:" << (int)input[i] << endl;
+    if (((input[i] >= 0 && input[i] <= 39) || (input[i] == 44) || (input[i] >= 58 &&
+	 input[i] <= 93) || (input[i] >= 95 && input[i] <= 126))&& input[i] != 0 &&
+	input[i] != 32) {
+      cout << "Only numbers and operators." << endl;
+      }
+
+    else if (input[i] == 0) {
+      break;
+    }
+
+    //moves around the numbers and operators
+    for (int i = 0; i < 100; i++) {
+      if ((input[i] >= 48 && input[i] <= 57) || (input[i] >= 40 && input[i] <= 47)
+	  || (input[i] == 94) && (input[i] == '+')) {
+
+	char temp = input[i];
+	int j = i;
+	while (j < strlen(input) - 1) {
+      cout << endl << "------------------------";
+      input[j] = input[j+1];
+      j++;
+      cout << endl << "Answer: " << input[j] << input[i] << endl;
+      break;
+	}
+      }
+    }
+
+    if (dq = NULL) {
+      dq = new Tree();
+      dq -> q = NULL;
+      dq -> num = new Tree[50];
+    }
+    else {
+      
+    }
+
+    if (strcmp(input, "00") == 0) {
+      peek(list);
+      }
+    }
+  }
+}
+
+//stack: looks at the data
+int peek(char* list) {
+    cout << list << endl;
+}
+
+//queue: fills in the line of data
+void enqueue() {
+
+}
+
+//queue: outputs the data
+int dequeue() {
+
+}
