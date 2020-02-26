7e3fa02a81d34aa30306f7a2476f5af1a7155da9
ninja2002a <52730345+ninja2002a@users.noreply.github.com>
2020-02-26 10:49:00 -0800

    Add files via upload

---
+++ heap.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int max;
+  int min;
+  char input[30];
+  int* a = new int[1];
+  int* b = new int[1];
+  int* c = new int[1];
+
+  cin >> input;
+  
+  for (int i = 0; i < 5; i++) {
+    
+
+    cout << "[" << a << "] " << "[" << b << "] " << "[" << c << "]"; 
+  }
+}
