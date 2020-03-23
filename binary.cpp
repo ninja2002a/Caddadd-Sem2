ee6b63a224e2f36f150311d643d350ff8b31087e
ninja2002a <52730345+ninja2002a@users.noreply.github.com>
2020-03-23 14:26:48 -0700

    Add files via upload

---
+++ binary.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+  Binary is node based data structure
+  2 childs (nodes)
+  It is a sorting and searching algorithm to construct abstract data
+  Another linked list
+  Right is bigger than left
+  Only need one .h file
+*/
+
+int main() {
+
+}
---
+++ left.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include "left.h"
+
+using namesapce std;
